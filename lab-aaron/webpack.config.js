e3045346d71b6dfa8be55f1dcf43c33c7d228ec9
Aaron McPeek <arn1313@comcast.net>
2017-10-02 16:34:32 -0700

    making sure i have no module issues

---
+++ lab-aaron/webpack.config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  devServer: {
+    'historyApiFallBack': true,
+  },
+  entry
+}

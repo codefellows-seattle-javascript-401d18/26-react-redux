5e0dc0a26aa529aa23db4b018beb70b8dcfbe720
Aaron McPeek <arn1313@comcast.net>
2017-10-02 16:44:28 -0700

    hacked the webpack in

--- lab-aaron/webpack.config.js
+++ lab-aaron/webpack.config.js
@@ -8,5 +8,27 @@ module.exports = {
   devServer: {
     'historyApiFallBack': true,
   },
-  entry
-}
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle-[hash].js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlPlugin({ template: `${__dirname}/src/index.html`}),
+    new ExtractTextPlugin('bundle-[hash].css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: 'babel-loader',
+      }
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+};

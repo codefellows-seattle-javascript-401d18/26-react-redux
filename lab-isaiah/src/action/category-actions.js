3de421f39b4a9428e44cc2955218d0566599fe6e
lonewalker72 <walkisai.72@gmail.com>
2017-10-03 09:03:30 -0700

    lab-isaiah

--- lab-isaiah/src/action/category-actions.js
+++ lab-isaiah/src/action/category-actions.js
@@ -4,6 +4,13 @@ export const categoryCreate = category => {
   category.id = uuid();
   category.timestamp = new Date();
   return {
+    type: 'CATEGORY_CREATE',
+    payload: category,
+  };
+};
+
+export const categoryUpdate = category => {
+  return {
     type: 'CATEGORY_UPDATE',
     payload: category,
   };

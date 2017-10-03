d61d4b2853a301384d1cb43d77c4d293d5ed5a9f
Aaron McPeek <arn1313@comcast.net>
2017-10-02 18:15:01 -0700

    added more file structure and the expense-action.js

---
+++ lab-aaron/src/action/expense-actions.js
@@ -0,0 +1,25 @@
+import uuid from 'uuid/v4';
+
+//make changes as you add to make it expense appropriate
+export const expenseCreate = expense => {
+  expense.id = uuid();
+  expense.timestamp = new Date();
+  return {
+    type: 'EXPENSE_CREATE',
+    payload: expense,
+  };
+};
+
+export const expenseUpdate = expense => {
+  return {
+    type: 'EXPENSE_UPDATE',
+    payload: expense,
+  };
+};
+
+export const expenseDelete = expense => {
+  return {
+      type: 'EXPENSE_DELETE',
+      payload: exense,
+  };
+};

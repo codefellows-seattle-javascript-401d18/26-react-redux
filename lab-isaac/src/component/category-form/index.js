8972089028cc67b6b7fe0c52305dfc95e218980a
esack7 <isaacheist@gmail.com>
2017-10-03 08:24:59 -0700

    added budget input to form

--- lab-isaac/src/component/category-form/index.js
+++ lab-isaac/src/component/category-form/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import uuid from 'uuid/v4';
 
 class CategoryForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: props.category ? props.category.title : '',
+      title: '',
+      budget: 0,
+      // id: uuid(),
+      // timestamp: new Date(),
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ title: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
     this.props.onComplete(Object.assign({}, this.state));
+    this.setState({
+      title: '',
+      budget: 0,
+    });
   }
 
   render() {
@@ -27,6 +35,11 @@ class CategoryForm extends React.Component {
           name="title"
           placeholder="enter a budget category"
           value={this.state.title}
+          onChange={this.handleChange} />
+        <input
+          type="number"
+          name="budget"
+          value={this.state.budget}
           onChange={this.handleChange} />
         <button type="submit">{this.props.buttonText}</button>
       </form>

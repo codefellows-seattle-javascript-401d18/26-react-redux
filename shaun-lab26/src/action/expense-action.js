48f380eff14048c2c0120335686bd4b4caee7bb8
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-03 21:04:40 -0700

    adds expense item in component

---
+++ shaun-lab26/src/action/expense-action.js
@@ -0,0 +1,22 @@
+import uuid from 'uuid/v4';
+
+export const expenseCreate = expense => {
+  return {
+    type: 'EXPENSE_CREATE',
+    payload: {...expense, id: uuid(), timestamp: new Date()},
+  };
+};
+
+export const expenseUpdate = expense => {
+  return {
+    type: 'EXPENSE_UPDATE',
+    payload: {...expense},
+  };
+};
+
+export const expenseDelete = expense => {
+  return {
+    type: 'EXPENSE_DELETE',
+    payload: expense,
+  };
+};

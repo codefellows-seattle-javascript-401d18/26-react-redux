25c79677496dca226d8c1b540b5e198d81499755
saidmattar <ssabdou@gmail.com>
2017-10-03 19:02:17 -0700

    wrong branch

--- lab-said/src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import expensesReducer from './expense';
-import categoriesReducer from './category';
-
-export default combineReducers({
-  categories: categoriesReducer,
-  expenses: expensesReducer,
-});

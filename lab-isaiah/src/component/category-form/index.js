2578d75f44a7b807a25ceba8128eda3b3be97884
lonewalker72 <walkisai.72@gmail.com>
2017-10-03 22:17:42 -0700

    lab-isaiah

--- lab-isaiah/src/component/category-form/index.js
+++ lab-isaiah/src/component/category-form/index.js
@@ -1,11 +1,13 @@
+import './_category-form.scss';
 import React from 'react';
-
 
 class CategoryForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: props.category ? props.category.title : '',
+      id: props.category ? props.category.id : null,
+      timestamp: props.category ? props.category.timestatmp : null,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,16 +20,20 @@ class CategoryForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    console.log('isaiah was here', this.state);
     this.props.onComplete(Object.assign({}, this.state));
+    this.setState({title: ''});
   }
 
   render() {
     return (
       <form className="category-form" onSubmit={this.handleSubmit}>
         <input
+          className="cateForm"
           type="text"
           name="title"
           placeholder="enter a title"
+          required
           value={this.state.title}
           onChange={this.handleChange}/>
 

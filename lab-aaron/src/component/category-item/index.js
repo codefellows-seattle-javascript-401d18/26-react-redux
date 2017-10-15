dc3e8f12e187cf1e60b75c3709e6d2912799c973
Aaron McPeek <arn1313@comcast.net>
2017-10-15 14:37:49 -0700

    added in up to lab28, trying to bug smash

--- lab-aaron/src/component/category-item/index.js
+++ lab-aaron/src/component/category-item/index.js
@@ -1,38 +1,81 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import CategoryForm from '../category-form';
+import ExpenseForm from '../expense-form';
+import ExpenseItem from '../expense-item';
+import {connect} from 'react-redux';
 import {categoryUpdate, categoryDelete} from '../../action/category-actions';
+import {expenseUpdate, expenseDelete, expenseCreate} from '../../action/expense-action';
 
 class CategoryItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      editCategory: false,
+      expenseBox: false,
+    };
+    this.toggleCategory = this.toggleCategory.bind(this);
+    this.toggleExpense = this.toggleExpense.bind(this);
+  }
+
+  toggleCategory() {
+    this.setState({editCategory: !this.state.editCategory});
+  }
+
+  toggleExpense(){
+    this.setState({expenseBox: !this.state.expenseBox});
   }
 
   render() {
+    let {category, categoryUpdate, categoryDelete, expense, expenses} = this.props;
     return (
-      <div className="category-item">
-        <button onClick={() => this.props.categoryDelete(this.props.category)}>X</button>
-        <h3>{this.props.category.title}</h3>
-        <CategoryForm
-          buttonText="update"
-          onComplete={this.props.categoryUpdate}
-          category={this.props.category}/>
-      </div>
+      <section>
+        <div className="expense-list">
+          <h2>{category.title}</h2>
+          <h3>Budget: {category.budget}</h3>
+          <button onClick={() => this.props.categoryDelete(this.props.category)}>Delete</button>
+          <button onClick={this.toggleCategory}>Edit</button>
+          {this.state.editCategory ?
+            <CategoryForm
+              buttonText="update"
+              category={category}
+              onComplete={this.props.categoryUpdate}
+            />
+            :
+            undefined
+          }
+          <div className='expense-container'>
+            <ExpenseForm
+              categoryId={category.id}
+              buttonText='New Expense'
+              onComplete={this.props.expenseCreate}
+            />
+
+            <div className='expense-items'>
+              {this.props.expenses.map(expense =>
+                <p>
+                  <ExpenseItem key={expense.id} expense={expense} category={this.props} />
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
-let mapStateToProps = state => {
+let mapStateToProps = (state, props) => {
   return {
-    categories: state,
+    expenses: state.expenses[props.category.id],
   };
 };
 
 let mapDispatchToProps = (dispatch, getState) => {
   return {
-    categoryUpdate: category => dispatch(categoryUpdate(category)),
-    categoryDelete: category => dispatch(categoryDelete(category)),
+    categoryUpdate: (category) => dispatch(categoryUpdate(category)),
+    categoryDelete: (category) => dispatch(categoryDelete(category)),
+    expenseCreate: (expense) => dispatch(expenseCreate(expense)),
   };
 };
 
-export default connect(null, mapDispatchToProps)(CategoryItem);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryItem);
--- lab-aaron/src/component/dashboard-container/index.js
+++ lab-aaron/src/component/dashboard-container/index.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {categoryCreate} from '../../action/category-actions';
+import {
+  categoryCreate,
+  categoryUpdate,
+  categoryDelete,
+} from '../../action/category-actions';
 import CategoryForm from '../category-form';
 import CategoryItem from '../category-item';
 
 class DashBoardContainer extends React.Component {
+
   componentDidMount() {
-    console.log('__DASHBOARD__'. this);
-    // this.props.categoryCreate({title: 'Create a Category'});
-    this.props.categoryCreate({title: 'Debit Card Purchases'});
-    this.props.categoryCreate({title: 'Mortgage/Rent'});
+    this.props.categoryCreate({title: 'Mortgage/Rent', budget: '1500'});
   }
 
   render() {
     return (
-      <main className="main-content">
-        <h2>Expense Tracker</h2>
-        <CategoryForm
-          buttontext="create"
-          onComplete={this.props.categoryCreate}/>
-
-        {this.props.categories.length ?
-          <div>
-            {this.props.categories.map(item => {
-              return <CategoryItem
+      <main className="dashboard-container">
+        <h1>Expense Tracker</h1>
+        <h2>Total Budget: {this.props.totalMonthly}</h2>
+        <h2>Remaining Budget: {this.props.totalRemaining}</h2>
+        <div className='category-form'>
+          <CategoryForm
+            buttontext="create"
+            onComplete={this.props.categoryCreate} />
+        </div>
+        <div className='category-list'>
+          {this.props.categories.map(item => {
+            return (
+              <CategoryItem
                 key={item.id}
-                category={item}/>;
-            })}
-          </div> :
-          <h2>Add some Expense Categories</h2>
-        }
+                category={item}
+              />
+            );
+          })}
+        </div>
       </main>
     );
   }
@@ -37,13 +42,16 @@ class DashBoardContainer extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    categories: state,
+    categories: state.categories,
+    totalMonthly: 3000,
   };
 };
 
 const mapDispatchToProps = (dispatch, getState) => {
   return {
     categoryCreate: category => dispatch(categoryCreate(category)),
+    categoryUpdate: category => dispatch(categoryUpdate(category)),
+    categoryDelete: category => dispatch(categoryDelete(category)),
   };
 };
 
